7c869513345d205b6781ea4882f7410563c835cd
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-04-30 22:38:43 -0400

    Solves error with user seed data

--- migrations/20230428155448-create-user.js
+++ migrations/20230428155448-create-user.js
@@ -15,6 +15,9 @@ module.exports = {
       password: {
         type: Sequelize.STRING
       },
+      email: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
